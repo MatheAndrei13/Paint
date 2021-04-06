4f084013f73ac786941ef13bafea87cb79cffefc
Andrei Mathe <matheandrei13.me@gmail.com>
2021-04-06 16:57:35 +0300

    Reading the size of app window from file

--- Paint/src/Main.cpp
+++ Paint/src/Main.cpp
@@ -2,8 +2,15 @@
 #include <fstream>
 
 int main() {
+	COORD appSize = { 120, 30 };
+	COORD fontSize = { 8, 16 };
+
+	std::ifstream in("config.txt");
+	in >> appSize.X >> appSize.Y >> fontSize.X >> fontSize.Y;
+	in.close();
+
 	Paint paint;
-	paint.Create(L"Paint", 120, 30, 8, 16);
+	paint.Create(L"Paint", appSize.X, appSize.Y, fontSize.X, fontSize.Y);
 	paint.Start();
 	return 0;
 }
