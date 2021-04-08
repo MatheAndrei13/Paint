bc7d84e63b582f10a77ad498944448952533dc3a
Andrei Mathe <matheandrei13.me@gmail.com>
2021-04-08 14:18:30 +0300

    Added TODO list

---
+++ Paint/src/TODOList.cpp
@@ -0,0 +1,9 @@
+// TODO: Add an option to change the size of the font while the app is running
+// TODO: Add color pallet for foreground and backgroud
+// TODO: Add glyph pallet
+// TODO: Add tools - oval tool, fill tool, copy tool, paste tool
+// TODO: Add shortcut list
+// TODO: Add canvas resize option and draging the canvas
+// TODO: Add exporting option to PNG
+// TODO: Add my own image format
+// TODO: Add save and load functions for images
